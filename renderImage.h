097c98ace238c11e082670bf7e64153f20bbc240
Keith Kinnard <kkinnard@nd.edu>
2015-04-22 15:10:39 -0400

    Adding the code for rendering the bar so it can be added to the main program

---
+++ renderImage.h
@@ -0,0 +1,12 @@
+void ~::render(int x, int y, SDL_Rect* clip=NULL){
+	SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+
+	if (clip!=NULL){
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+	SDL_RenderCopy(gameRenderer, mTexture, clip, &renderQuad)
+}
+	SDL_SetRenderDrawColow(gameRenderer, 0xFF);
+	SDL_RenderClear(gRenderer);
+	gSpriteSheetTexture.render(x, y, width, height);	
